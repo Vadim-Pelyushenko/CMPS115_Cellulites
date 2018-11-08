8fdeba4e324fe675a8c30e46c329e92c2e8bac8a
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-11-07 17:31:05 -0800

    More Examples

--- Workable Automata/Version 4/game_of_life.js
+++ Workable Automata/Version 4/game_of_life.js
@@ -112,6 +112,136 @@ function gameOfLifeDrawBoard()
 	}
 }
 
+function gameOfLifeUpdateCell2(posR,posC)
+{
+	let count = 0;
+
+	let grid = this.grid;
+	let rows = this.rows;
+	let cols = this.cols;
+	let cell = grid[posR][posC];
+
+	let up = posR - 1;
+	let down = posR + 1;
+	let left = posC - 1;
+	let right = posC + 1;
+
+	// COUNTING LIVE NEIGHBORS
+	// upper three neighbors
+	if(up >= 0 && left >= 0 && grid[up][left].state[0] == 1)
+		count++;
+
+	if(up >= 0 && grid[up][posC].state[0] == 1)
+		count++;
+
+	if(up >= 0 && right < cols && grid[up][right].state[0] == 1)
+		count++;
+
+	// middle two neighbors
+	if(left >= 0 && grid[posR][left].state[0] == 1)
+		count++;
+
+	if(right < cols && grid[posR][right].state[0] == 1)
+		count++;
+
+	// bottom three neighbors
+	if(down < rows && left >= 0 && grid[down][left].state[0] == 1)
+		count++;
+
+	if(down < rows && grid[down][posC].state[0] == 1)
+		count++;
+
+	if(down < rows && right < cols && grid[down][right].state[0] == 1)
+		count++;
+
+
+	// Updating state
+	let nextState;
+
+	if(cell.state[0] == 0)
+	{
+		if(count == 3)
+			nextState = 1;
+		else
+			nextState = 0;
+	}
+	else
+	{
+		if(count < 2 || count > 4)
+			nextState = 0;
+		else
+			nextState = 1;
+	}
+
+	// console.log("Live neighbors: " + count + "\n\n");
+	cell.setFutureState(0,nextState);
+}
+
+function gameOfLifeUpdateCell3(posR,posC)
+{
+	let count = 0;
+
+	let grid = this.grid;
+	let rows = this.rows;
+	let cols = this.cols;
+	let cell = grid[posR][posC];
+
+	let up = posR - 1;
+	let down = posR + 1;
+	let left = posC - 1;
+	let right = posC + 1;
+
+	// COUNTING LIVE NEIGHBORS
+	// upper three neighbors
+	if(up >= 0 && left >= 0 && grid[up][left].state[0] == 0)
+		count++;
+
+	if(up >= 0 && grid[up][posC].state[0] == 0)
+		count++;
+
+	if(up >= 0 && right < cols && grid[up][right].state[0] == 0)
+		count++;
+
+	// middle two neighbors
+	if(left >= 0 && grid[posR][left].state[0] == 0)
+		count++;
+
+	if(right < cols && grid[posR][right].state[0] == 0)
+		count++;
+
+	// bottom three neighbors
+	if(down < rows && left >= 0 && grid[down][left].state[0] == 0)
+		count++;
+
+	if(down < rows && grid[down][posC].state[0] == 0)
+		count++;
+
+	if(down < rows && right < cols && grid[down][right].state[0] == 0)
+		count++;
+
+
+	// Updating state
+	let nextState;
+
+	if(cell.state[0] == 1)
+	{
+		if(count == 3)
+			nextState = 0;
+		else
+			nextState = 1;
+	}
+	else
+	{
+		if(count < 2 || count > 3)
+			nextState = 1;
+		else
+			nextState = 0;
+	}
+
+	// console.log("Live neighbors: " + count + "\n\n");
+	cell.setFutureState(0,nextState);
+}
+
 function gameOfLifeDrawBoard2()
 {
 	// For easier reference
