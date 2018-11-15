f71f0a75f97a19ad33e2f762c154c60cf346aac6
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-11-14 22:53:09 -0800

    True revert

--- Workable Automata/Version 4/board.js
+++ Workable Automata/Version 4/board.js
@@ -9,6 +9,7 @@ class Board
 	// The number of columns that there are
 	//
 	// grid
+	
 	// The entire grid of cells
 	//
 	// initBoard
@@ -115,8 +116,6 @@ class Board
 			console.log(msg);
 			return;
 		}
-
-		// printGameOfLifeBoard(this.grid);
 
 		for(let r = 0; r < this.rows; r++)
 		{
