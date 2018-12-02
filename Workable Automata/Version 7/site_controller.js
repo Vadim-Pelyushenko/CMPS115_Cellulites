e0ec39f7e19061ef857b525d8a01a0691b78f435
Vadim-Pelyushenko <37136767+Vadim-Pelyushenko@users.noreply.github.com>
2018-12-02 14:20:22 -0800

    File Loading/Saving Implemented

--- Workable Automata/Version 7/site_controller.js
+++ Workable Automata/Version 7/site_controller.js
@@ -288,3 +288,45 @@ function setDrawingEventListeners()
 	targetCanvas.onmouseup = zoom.targOnMouseUp.bind(zoom);
 	targetCanvas.onmouseleave = zoom.targOnMouseUp.bind(zoom);
 }
+
+// FILE STUFF
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
+// ---------------------------------------------------------------
+
+function saveTextAsFile()
+{
+	let textToSave = document.getElementById("functionInputArea").value;
+	let textToSaveAsBlob = new Blob([textToSave],{type:"text/plain"});
+	let textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+	let fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
+
+	let downloadLink = document.createElement("a");
+	downloadLink.download = fileNameToSaveAs;
+	downloadLink.innerHTML = "Download File";
+	downloadLink.href = textToSaveAsURL;
+	downloadLink.onclick = destroyClickedElement;
+	downloadLink.style.display = "none";
+	document.body.appendChild(downloadLink);
+
+	downloadLink.click();
+}
+
+function destroyClickedElement(event)
+{
+	document.body.removeChild(event.target);
+}
+
+function loadFileAsText()
+{
+	let fileToLoad = document.getElementById("fileToLoad").files[0];
+
+	let fileReader = new FileReader();
+	fileReader = new FileReader();
+	fileReader.onload = function(fileLoadedEvent)
+	{
+		let textFromFileLoaded = fileLoadedEvent.target.result;
+		document.getElementById("functionInputArea").value = textFromFileLoaded;
+	};
+	fileReader.readAsText(fileToLoad, "UTF-8");
+}
